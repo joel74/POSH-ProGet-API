dff438d8fd9e08c9ba0db7e7301224884d7d8d11
joel <jnewton@springcm.com>
2016-04-13 13:48:12 -0500

    New functions: Get-Package, Get-FeedID, Remove-PackageVersionFromFeed Adding NuGet.exe and NuGet.ServerExtensions.dll to be able to copy packages between feeds.

--- ProGet.psm1
+++ ProGet.psm1
@@ -7,7 +7,7 @@
     File Name    : ProGet.psm1
     Author       : Joel Newton - jnewton@springcm.com
     Requires     : PowerShell V3
-    Dependencies : (none)
+    Dependencies : Nuget.exe, NuGet.ServerExtensions.dll
 
 #>
 
@@ -18,29 +18,68 @@ $ScriptLocation = split-path -parent $MyInvocation.MyCommand.Path
 $ProGetSettings = Get-Content -Path "$ScriptLocation\Settings.json" | Out-String | ConvertFrom-Json
 
 $API_KEY = $ProGetSettings.API_KEY
+$API_BaseURI = "$ProGetServerURL/api/json"
+
 $NuGet_API_Key = $ProGetSettings.NuGet_API_Key
+
 ######################
+
 
 Function Get-FeedList {
 
+    [CmdletBinding()]
     param (
    	    [Parameter(Mandatory=$true)]
         $ProGetServerURL
     )
 
-    $API_BaseURI = "$ProGetServerURL/api/json"
+    begin {
 
-    #Retrieve the package and version requested
-    $URI = "$API_BaseURI/Feeds_GetFeeds?API_Key=$API_KEY"
-    $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
-    $Feeds = $Request.Content | ConvertFrom-Json
-    $Feeds.Feed_Name
+    }
+
+    process {
+        #Retrieve the package and version requested
+        $URI = "$API_BaseURI/Feeds_GetFeeds?API_Key=$API_KEY"
+        $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
+        $Feeds = $Request.Content | ConvertFrom-Json
+        $Feeds.Feed_Name
+
+    }
+}
+
+
+Function Get-FeedID{
+
+    
+    [CmdletBinding()]
+    param (
+
+   	    [Parameter(Mandatory=$true)]
+        $ProGetServerURL,
+        [Parameter(Mandatory=$true)]
+        $FeedName
+    )
+
+    begin {
+
+    }
+
+    process {
+
+        #Retrieve the ID for the feed
+        $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
+        $Feed = $Request.Content | ConvertFrom-Json
+
+        Write-Output($Feed.Feed_Id)
+
+    }
 
 }
 
 
 Function Test-PackageIsInFeed {
 
+    [CmdletBinding()]
     param (
     
    	    [Parameter(Mandatory=$true)]
@@ -52,35 +91,41 @@ Function Test-PackageIsInFeed {
 
     )
 
-    $API_BaseURI = "$ProGetServerURL/api/json"
-
-    #Retrieve the ID for the feed
-    $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
-    $Feed = $Request.Content | ConvertFrom-Json
-
-    #Retrieve the package and version requested
-    $URI = "$API_BaseURI/NuGetPackages_GetLatest?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id + "&PackageIds_Psv=$PackageName"
-    $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
-    $Package = $Request.Content | ConvertFrom-Json
-
-    #If it is found, return TRUE
-    If ($Package){
-
-        Write-Verbose "$PackageName package found at $API_BaseURI"
-        Write-Output $true
-    }
-    #Otherwise, return false
-    Else {
+    begin {
     
-        Write-Verbose "$PackageName package not found at $API_BaseURI"
-        Write-Output $false
     }
 
+    process {
+
+        #Retrieve the ID for the feed
+        $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
+        $Feed = $Request.Content | ConvertFrom-Json
+
+        #Retrieve the package and version requested
+        $URI = "$API_BaseURI/NuGetPackages_GetLatest?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id + "&PackageIds_Psv=$PackageName"
+        $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
+        $Package = $Request.Content | ConvertFrom-Json
+
+        #If it is found, return TRUE
+        If ($Package){
+
+            Write-Verbose "$PackageName package found at $API_BaseURI"
+            Write-Output $true
+        }
+        #Otherwise, return false
+        Else {
+    
+            Write-Verbose "$PackageName package not found at $API_BaseURI"
+            Write-Output $false
+        }
+
+    }
 }
 
 
 Function Test-PackageVersionIsInFeed {
 
+    [CmdletBinding()]
     param (
 
    	    [Parameter(Mandatory=$true)]
@@ -93,35 +138,77 @@ Function Test-PackageVersionIsInFeed {
         $PackageVersion
     )
 
-    $API_BaseURI = "$ProGetServerURL/api/json"
-
-    #Retrieve the ID for the feed
-    $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
-    $Feed = $Request.Content | ConvertFrom-Json
-
-    #Retrieve the package and version requested
-    $URI = "$API_BaseURI/NuGetPackages_GetPackage?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id + "&Package_Id=$PackageName&Version_Text=$PackageVersion"
-    $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
-    $Package = $Request.Content | ConvertFrom-Json
-
-    #If they exist, return TRUE
-    If ($Package.NuGetPackages_Extended){
-
-        Write-Verbose "$PackageName version $Package.Version_Text found at $API_BaseURI"
-        Write-Output $true
+    begin {
+ 
     }
-    #Otherwise, return false
-    Else {
+
+    process {
+
+        #Retrieve the ID for the feed
+        $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
+        $Feed = $Request.Content | ConvertFrom-Json
+
+        #Retrieve the package and version requested
+        $URI = "$API_BaseURI/NuGetPackages_GetPackage?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id + "&Package_Id=$PackageName&Version_Text=$PackageVersion"
+        $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
+        $Package = $Request.Content | ConvertFrom-Json
+
+        #If they exist, return TRUE
+        If ($Package.NuGetPackages_Extended){
+
+            Write-Verbose "$PackageName version $Package.Version_Text found at $API_BaseURI"
+            Write-Output $true
+        }
+        #Otherwise, return false
+        Else {
     
-        Write-Verbose "$PackageName version $PackageVersion not found at $API_BaseURI"
-        Write-Output $false
+            Write-Verbose "$PackageName version $PackageVersion not found at $API_BaseURI"
+            Write-Output $false
+        }
+
+    }
+}
+
+Function Remove-PackageVersionFromFeed {
+
+    [CmdletBinding(SupportsShouldProcess=$true,
+        ConfirmImpact="High")]
+    param (
+
+   	    [Parameter(Mandatory=$true)]
+        $ProGetServerURL,
+        [Parameter(Mandatory=$true)]
+        $FeedName,
+        [Parameter(Mandatory=$true)]
+        $PackageName,
+        [Parameter(Mandatory=$true)]
+        $PackageVersion,
+        [Parameter(Mandatory=$true)][System.Management.Automation.PSCredential]
+        $Credentials
+       
+    )
+
+    begin {
+
+        $URI = "$ProGetServerURL/nuget/$FeedName/$PackageName/$PackageVersion"
+    
     }
 
+    process {
+
+        If ($PScmdlet.ShouldProcess("Delete this package version?")){
+            Invoke-RestMethod -Method Delete -Uri "$URI" -Credential $Credentials -Headers @{"Content-Type"="text/html";"X-NuGet-ApiKey"="$NuGet_API_Key"}
+        }
+
+    }
 }
+
+
 
 
 Function Get-LatestPackageVersion {
 
+    [CmdletBinding()]
     param (
    	    [Parameter(Mandatory=$true)]
         $ProGetServerURL,
@@ -131,23 +218,29 @@ Function Get-LatestPackageVersion {
         $PackageName
     )
 
-    $API_BaseURI = "$ProGetServerURL/api/json"
+    begin {
 
-    #Retrieve the ID for the feed
-    $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
-    $Feed = $Request.Content | ConvertFrom-Json
+    }
 
-    #Retrieve the package and version requested
-    $URI = "$API_BaseURI/NuGetPackages_GetLatest?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id + "&PackageIds_Psv=$PackageName"
-    $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
-    $PackageVersion = $Request.Content | ConvertFrom-Json
+    process {
 
-    Write-Output($PackageVersion.Version_Text)
+        #Retrieve the ID for the feed
+        $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
+        $Feed = $Request.Content | ConvertFrom-Json
 
+        #Retrieve the package and version requested
+        $URI = "$API_BaseURI/NuGetPackages_GetLatest?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id + "&PackageIds_Psv=$PackageName"
+        $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
+        $PackageVersion = $Request.Content | ConvertFrom-Json
+
+        Write-Output($PackageVersion.Version_Text)
+
+    }
 }
 
 Function Get-Latest {
 
+    [CmdletBinding()]
     param (
    	    [Parameter(Mandatory=$true)]
         $ProGetServerURL,
@@ -155,23 +248,29 @@ Function Get-Latest {
         $FeedName
     )
 
-    $API_BaseURI = "$ProGetServerURL/api/json"
+    begin {
 
-    #Retrieve the ID for the feed
-    $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
-    $Feed = $Request.Content | ConvertFrom-Json
+    }
 
-    #Retrieve the package and version requested
-    $URI = "$API_BaseURI/NuGetPackages_GetLatest?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id 
-    $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
-    $PackageVersion = $Request.Content | ConvertFrom-Json
+    process {
 
-    Write-Output($PackageVersion)
+        #Retrieve the ID for the feed
+        $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
+        $Feed = $Request.Content | ConvertFrom-Json
 
+        #Retrieve the package and version requested
+        $URI = "$API_BaseURI/NuGetPackages_GetLatest?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id 
+        $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
+        $PackageVersion = $Request.Content | ConvertFrom-Json
+
+        Write-Output($PackageVersion)
+
+    }
 }
 
-Function Get-AllPackageVersions{
+Function Get-Package{
 
+    [CmdletBinding()]
     param (
    	    [Parameter(Mandatory=$true)]
         $ProGetServerURL,
@@ -181,46 +280,46 @@ Function Get-AllPackageVersions{
         $PackageName
     )
 
-    $API_BaseURI = "$ProGetServerURL/api/json"
+    begin{
 
-    #Retrieve the ID for the feed
-    $Request = Invoke-WebRequest -Uri "$API_BaseURI/Feeds_GetFeed?API_Key=$API_KEY&Feed_Name=$FeedName" -UseBasicParsing
-    $Feed = $Request.Content | ConvertFrom-Json
+        $URI = "$ProGetServerURL/nuget/$FeedName/Packages()?`$format=json&`$filter=Id eq '$PackageName'"
 
-    #Retrieve the package and version requested
-    $URI = "$API_BaseURI/NuGetPackages_GetPackage?API_Key=$API_KEY&Feed_Id=" + $Feed.Feed_Id + "&PackageIds_Psv=$PackageName"
-    $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
-    $PackageVersion = $Request.Content | ConvertFrom-Json
+    }
 
-    Write-Output($PackageVersion.Version_Text)
+    process {
+
+        $Request = Invoke-WebRequest -Uri $URI -UseBasicParsing
+
+        $PackageVersion = $Request.Content | ConvertFrom-Json
+
+        Write-Output($PackageVersion.d.results)
+    }
 }
 
 
 
 Function Copy-PackageToFeed {
 <#
-AUTHOR: Joel Newton
-CREATED DATE: 10/19/15
-LAST UPDATED DATE: 3/22/16
-VERSION: 1.1
-
-SYNOPSIS
+.SYNOPSIS
    This script retrieves the latest versions of the packages on the specified origin feed and pushes them to the specified destination feed
 	
-PARAMETER 
+.NOTES
+    PARAMETERS:
+    $ProGetServerURL
     $PackageName
     $PackageVersion
     $OriginFeedName
     $DestinationFeedName
 
-EXAMPLE
-    
-    Copy-PackageToFeed -PackageName Web -PackageVersion 1.16.2.35400 -OriginFeedName Branch -DestinationFeedName AtlasQARelease -ProGetServerURL http://progetna21.atlas.cm.com
+.EXAMPLE
+    Copy-PackageToFeed -ProGetServerURL http://progetserver -PackageName Web -PackageVersion 1.1.0 -OriginFeedName Branch -DestinationFeedName Release
 
 #>
     [CmdletBinding()]
     param(
  
+   	    [Parameter(Mandatory=$true)]
+        $ProGetServerURL,
 	    [Parameter(Mandatory=$true)]
         [string[]]$PackageList,
    	    [Parameter(Mandatory=$true)]
@@ -228,23 +327,18 @@ EXAMPLE
    	    [Parameter(Mandatory=$true)]
         $OriginFeedName,
    	    [Parameter(Mandatory=$true)]
-        $DestinationFeedName,
-   	    [Parameter(Mandatory=$true)]
-        $ProGetServerURL
+        $DestinationFeedName
     )
 
     begin{
 
-    }
-    process{
-
-
-        $API_BaseURI = "$ProGetServerURL/api/json"
-
         $SourceFeed = "$ProGetServerURL/nuget/$OriginFeedName"
         $ListTargetPackage = "$ProGetServerURL/nuget/$DestinationFeedName"
         $PushPackageURL = "$ProGetServerURL/api/v2/package/$DestinationFeedName"
-    
+
+    }
+    process{
+
         ForEach ($PackageName in $PackageList){
 
             #Make sure the package name and version exist before trying to promote
@@ -261,8 +355,7 @@ EXAMPLE
             $Feed = $Request.Content | ConvertFrom-Json
 
             Write-Output "Mirroring version $PackageVersion of $PackageName from $SourceFeed to $ListTargetPackage"
-            cmd /c c:\NuGet\v286-signed\nuget mirror $PackageName $ListTargetPackage $PushPackageURL -version $PackageVersion -source $SourceFeed -apikey $NuGet_API_Key -NoCache
-
+            cmd /c $ScriptLocation\bin\nuget.exe mirror $PackageName $ListTargetPackage $PushPackageURL -version $PackageVersion -source $SourceFeed -apikey $NuGet_API_Key -NoCache
 
         }
 
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # POSH-ProGet-API
-A PowerShell module for working with Inedo's ProGet API
+A PowerShell module for working with Inedo's ProGet API. 
+Currently, this is primarily for working nuget feeds.
+
+It requires PowerShell v3 or higher.
+
+It has a dependency on nuget.exe, so a copy has been included.
+
+The module contains the following functions:
+
+   * Copy-PackageToFeed
+   * Get-FeedID
+   * Get-FeedList
+   * Get-Latest
+   * Get-LatestPackageVersion
+   * Get-Package
+   * Remove-PackageVersionFromFeed
+   * Test-PackageIsInFeed
+   * Test-PackageVersionIsInFeed
